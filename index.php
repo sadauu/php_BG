219583f9c6650c188a888bf025bc948c0f62a9bb
ahmedsadaunamaiyaki <ahmedsadaunamaiyaki@gmail.com>
2023-10-17 20:57:10 +0100

    something light

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  $name = "abudlmalik sakariyahu";
+  
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SWE 205</title>
+</head>
+<body>
+    
+    <h1 style="color: yellow; backgroud-colour: black; padding: 20px;">welcome! <?=  $name ?></h1>
+    
+
+</body>
+</html>
