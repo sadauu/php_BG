2afb3ca86c9e6874369212962618cd7a2959bcdb
ahmedsadaunamaiyaki <ahmedsadaunamaiyaki@gmail.com>
2023-10-18 13:43:07 +0100

    functions in php

--- index.php
+++ index.php
@@ -1,6 +1,34 @@
 <?php
   $name = "abudlmalik sakariyahu";
-  
+        //strings in php
+  //echo strlen($name);
+  //echo strrev($name);
+  //echo str_word_count($name);
+ //echo str_replace('sakariyahu','sadiq',$name)
+
+ $age = 20;
+ $bmi = 12.5;
+ $male = true;
+ $cars = ["ahmed", "adamu", "äliyu", "benz"];
+ var_dump($age);
+ /* class user{
+    function user(){
+        $this->name = "ahmed";
+    }
+  }*/
+
+  function sum(){
+    $x = 30;
+    $y = 7;
+    $sum = $x + $y;
+
+    return $sum;
+  }
+  $addtion = sum();
+  /*$name = "(a,b)";
+  return "a,b";
+  $name = "ahmed,adam";*/
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,6 +40,16 @@
 <body>
     
     <h1 style="color: yellow; backgroud-colour: black; padding: 20px;">welcome! <?=  $name ?></h1>
+    <P>you will like the folloing names and cars</P>
+        <ul>
+        <?php foreach($cars as $car){?>
+        <li><?= $car ?></li>
+        <P><?php if($car == "benz"){?>
+            your favorite car is <?= $car?>
+        </P>
+        <?php }?>
+        
+        </ul>
     
 
 </body>
